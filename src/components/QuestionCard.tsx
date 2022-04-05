3b48f972227f232bd7b7bcaa1c5f7866c061c85b
MichaelGetuWorku <michael.getu.hh3221@gmail.com>
2022-04-05 18:10:00 +0300

    featch question and displayed them

---
+++ src/components/QuestionCard.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+type Props = {
+  question: string;
+  answers: string[];
+  callback: any;
+  userAnswer: any;
+  questionNr: number;
+  totalQuestions: number;
+};
+
+const QuestionCard: React.FC<Props> = ({
+  question,
+  answers,
+  callback,
+  userAnswer,
+  questionNr,
+  totalQuestions,
+}) => (
+  <div>
+    Question Card
+    <p className="number">
+      Question:{questionNr}/ {totalQuestions}
+      {/* what question the user is on */}
+    </p>
+    <p dangerouslySetInnerHTML={{ __html: question }} />
+    <div>
+      {answers.map((answer) => (
+        <div key={answer}>
+          <button disabled={userAnswer} onClick={callback}>
+            <span dangerouslySetInnerHTML={{ __html: answer }} />
+          </button>
+        </div>
+      ))}
+    </div>
+  </div>
+);
+
+export default QuestionCard;
